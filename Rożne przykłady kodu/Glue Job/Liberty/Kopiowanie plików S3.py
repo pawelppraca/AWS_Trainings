b01119161f94dc5fd094e404170725e65bb3bcb7
Pawel Parasyn <pawel.parasyn.praca@gmail.com>
2025-01-09 14:52:30 +0100

    Update

--- Rożne przykłady kodu/Glue Job/Liberty/Kopiowanie plików S3.py
+++ Rożne przykłady kodu/Glue Job/Liberty/Kopiowanie plików S3.py
@@ -36,6 +36,26 @@ def copy_s3_folder(source_bucket, source_prefix, dest_bucket, dest_prefix):
         print(f'Error copying objects: {e}')
 
 
+def move_s3_folder(source_bucket, source_prefix, dest_bucket, dest_prefix):
+    s3 = boto3.client('s3')
+    paginator = s3.get_paginator('list_objects_v2')
+    try:
+        for page in paginator.paginate(Bucket=source_bucket, Prefix=source_prefix):
+            for obj in page.get('Contents', []):
+                source_key = obj['Key']
+                dest_key = source_key.replace(source_prefix, dest_prefix, 1)
+                copy_source = {
+                    'Bucket': source_bucket,
+                    'Key': source_key
+                }
+                s3.copy_object(CopySource=copy_source,
+                               Bucket=dest_bucket, Key=dest_key)
+                s3.delete_object(Bucket=source_bucket, Key=source_key)
+                print(f'Successfully moved {source_key} to {dest_key}')
+    except botocore.exceptions.ClientError as e:
+        print(f'Error moving objects: {e}')
+
+
 def main():
     source_bucket = 'aws-ppp-sources'
     source_key = 'dane_RS/tabela_parquet'
@@ -44,14 +64,21 @@ def main():
 
     copy_s3_object(source_bucket, source_key, dest_bucket, dest_key)
 
-
-
     source_bucket = 'aws-ppp-sources'
     source_prefix = 'dane_RS/'
     dest_bucket = 'aws-ppp-destinations'
     dest_prefix = 'dane_docelowe/'
 
     copy_s3_folder(source_bucket, source_prefix, dest_bucket, dest_prefix)
+
+
+    source_bucket = 'aws-ppp-sources'
+    source_prefix = 'source_folder/'
+    dest_bucket = 'aws-ppp-destinations'
+    dest_prefix = 'dest_folder/'
+
+    move_s3_folder(source_bucket, source_prefix, dest_bucket, dest_prefix)
+
 
 if __name__ == "__main__":
     main()
