cbe6e9ebe77fdaba4780344c42fc0f697ef950ea
Pawel Parasyn <pawel.parasyn.praca@gmail.com>
2025-03-11 13:06:17 +0100

    Dodanie sprawdzania MASKING POLICY

---
+++ Rożne przykłady kodu/Redshift/ciekawe SQL.sql
@@ -0,0 +1,20 @@
+-- Sprawdzenie czy Masking Policy jest nałożona
+SELECT
+    t.table_schema,
+    t.table_name,
+    c.column_name,
+    m.masking_policy_name
+FROM
+    information_schema.columns c
+JOIN
+    pg_catalog.pg_masking_policies m
+    ON c.column_name = m.column_name
+JOIN
+    information_schema.tables t
+    ON t.table_schema = c.table_schema
+    AND t.table_name = c.table_name
+WHERE
+    t.table_type = 'BASE TABLE'
+    AND m.masking_policy_name IS NOT NULL
+    AND t.table_name = '<your_table_name>'  -- Replace with your table name
+    AND t.table_schema = '<your_schema_name>'; -- Replace with your schema name
